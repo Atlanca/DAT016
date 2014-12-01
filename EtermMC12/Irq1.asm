bf3be6c73af08bd0d32e7e0bb23a92db4c1d8c4e
Gabriel Alpsten <galpsten@hotmail.com>
2014-12-01 20:14:34 +0100

    U65

---
+++ EtermMC12/Irq1.asm
@@ -0,0 +1,33 @@
+Port1:	EQU	$400
+Port2:	EQU	$401
+
+	ORG	$1000
+main:
+; Nollställer variabler
+	CLR	Var1
+	CLR	Var2
+; Initiera avbrottsvektor IRQ
+	LDX	#IrqR
+	STX	$3FF2
+; Nollstäl I-bit, tillåt avbrott
+	CLI
+
+	
+; Huvudprogram:
+; Ökar Var1. 
+; Skriver ut Var1 och Var2
+main_loop:
+	INC	Var1
+	MOVB	Var1,Port1
+	MOVB	Var2,Port2
+	BRA	main_loop
+	
+; Avbrottsrutin
+; Ökar Var2
+IrqR:	INC 	Var2
+	RTI
+	
+; Variabler
+Var1:	RMB	1
+Var2:	RMB	1
+	
