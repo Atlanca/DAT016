ca1d22158cb6e8943db02a11bafa368b4e611b54
Gabriel Alpsten <galpsten@hotmail.com>
2014-12-16 14:38:02 +0100

    U109-112

---
+++ Lab 5/codegen/globals.c
@@ -0,0 +1,35 @@
+// U109
+short 	shortint;
+long	longint;
+int	justint;
+int	intvec[10];
+
+struct {
+	int s1;
+	char s2;
+	char *s3;
+} komplex;
+
+
+
+// U110
+int a;
+short int b;
+long int c;
+char d;
+
+// U111
+char cvec[128];
+int ivec[128];
+short sivec[128];
+
+// U112
+struct mystructtype{
+	int previd;
+	char *name;
+	int id;
+	short number;
+	int nextid;
+};
+struct mystructtype mystruct;
+char *char_pointer;
