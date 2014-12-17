53495070d07bd081799cf2b2079624ef740e1917
Gabriel Alpsten <galpsten@hotmail.com>
2014-12-17 23:05:03 +0100

    Enabled keyboard in main

--- Lab 5/main.c
+++ Lab 5/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include "drill.h"
+#include "keyboardML15.h"
 
 void main(){
-DoAuto();
+
+	while(1){
+		unsigned char key = get_key();
+		
+		switch(){
+		    case 0:
+			MotorStart();
+		       break;
+		    case 1:
+			MotorStop();
+		       break;
+		    case 2:
+			DrillDown();
+		       break;
+		    case 3:
+			DrillUp();
+		       break;
+		    case 4:
+			Nstep(3);
+		       break;
+		    case 5:
+			DrillHole();
+		       break;
+		    case 6:
+			RefPos();
+		       break;
+		    case 7:
+			Outzero();
+		       break;
+		}
+	}
 }
